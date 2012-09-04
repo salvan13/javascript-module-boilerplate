e1acd7fe762cafb29d1a73b634385b3b49b00bf8
Antonio Salvati <salvan13@gmail.com>
2012-09-04 12:11:05 +0200

    replaced tabs with 2 spaces

--- Module.js
+++ Module.js
@@ -2,19 +2,19 @@
 ; 
 var Module = (function(window, undefined){
 
-	var privateMethod = function(){
-		console.log("private");
-	}
+  var privateMethod = function(){
+    console.log("private");
+  }
 
-	var publicMethod = function(){
-		console.log("public");
-	}
+  var publicMethod = function(){
+    console.log("public");
+  }
 
-	/**
-	 * return public methods
-	 **/
-	return {
-		publicMethod: publicMethod
-	}
+  /**
+   * return public methods
+  **/
+  return {
+    publicMethod: publicMethod
+  }
 
 })(window);
